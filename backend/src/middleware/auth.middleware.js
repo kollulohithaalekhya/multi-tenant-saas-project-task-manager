3fb9b1c3273e74131b36813b4ee74dd2e63fabc5
kollulohithaalekhya <alekhyakollu@gmail.com>
2025-12-24 19:32:25 +0530

    implement jwt authentication middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const authenticate = (req, res, next) => {
   const header = req.headers.authorization;
+  console.log("AUTH HEADER:", req.headers.authorization);
   if (!header || !header.startsWith("Bearer ")) {
     return res.status(401).json({ success: false, message: "Token missing" });
   }
